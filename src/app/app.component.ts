4286b0994abcada7336b6154d6a015afabefbda5
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2022-03-28 17:00:47 -0300

    Diretiva Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,14 @@ import {  Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ' <app-data-biding></app-data-biding>         <router-outlet></router-outlet> ',
+  template: ' <app-diretivas-atributos> </app-diretivas-atributos>  <router-outlet></router-outlet> ',
+
+  /* ' <!-- app-diretivas-estruturais></app-diretivas-estruturais>--> <!-- app-diretivas-atributos> <h1>Aulas de Diretivas Atributo </h1>  <h3> Final da aula </h3>  </app-diretivas-atributos> -->   <router-outlet></router-outlet> ', */
 })
 export class AppComponent implements OnInit{
+  
+  public destruir: boolean = true;
+  
   
   
   constructor(){
@@ -15,4 +20,7 @@ export class AppComponent implements OnInit{
     
   }
 
+  public destruirComponent(){
+
+  }
 }
