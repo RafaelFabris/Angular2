769c8ad7e6e98e6ced8f1eee17ea234e048245f0
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2022-03-29 19:38:38 -0300

    Module Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {  Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ' <app-diretivas-atributos> </app-diretivas-atributos>  <router-outlet></router-outlet> ',
+  template: '<app-new-component> </app-new-component> <router-outlet></router-outlet> ',
 
   /* ' <!-- app-diretivas-estruturais></app-diretivas-estruturais>--> <!-- app-diretivas-atributos> <h1>Aulas de Diretivas Atributo </h1>  <h3> Final da aula </h3>  </app-diretivas-atributos> -->   <router-outlet></router-outlet> ', */
 })
