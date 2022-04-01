dc017170b133188f6a219d5a2ace6898210eb53e
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2022-04-01 16:07:33 -0300

    Observable + http-Get

--- src/app/services/food-list.service.ts
+++ src/app/services/food-list.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FoodList } from '../module/food-list';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodListService {
+
+  public emitEvent = new EventEmitter();
 
   private list: Array<string> = [
     "X Bacon",
@@ -11,15 +16,28 @@ export class FoodListService {
     "Ovo"
   ];
 
+  //http://localhost:3000/list-food
+  private url: string = "http://localhost:3000/";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  public foodList(){
-    return this.list;
+  public foodList(): Observable<FoodList> {
+    return this.http.get<FoodList>(`${this.url}list-food`)
+                      .pipe(
+                        res => res,
+                        error => error
+                      );
   }
 
   public foodListAdd(value: string){
+    this.foodListAlert(value);
     return this.list.push(value);
   }
+
+  public foodListAlert(value: string){
+    return this.emitEvent.emit(value);
+  }
+
+
 
 }
