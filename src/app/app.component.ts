c3e616fe3633e357d818691f1ee7f03fc8aadcc9
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2022-04-01 15:07:31 -0300

    Comunicação de componentes com Services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {  Component, OnInit,} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '  <app-food-list></app-food-list>  <router-outlet></router-outlet>'
+  template: ' <app-food-add></app-food-add> <app-food-list></app-food-list>  <router-outlet></router-outlet>'
 })
 export class AppComponent implements OnInit{
 
--- src/app/services/food-list.service.ts
+++ src/app/services/food-list.service.ts
@@ -17,4 +17,9 @@ export class FoodListService {
   public foodList(){
     return this.list;
   }
+
+  public foodListAdd(value: string){
+    return this.list.push(value);
+  }
+
 }
