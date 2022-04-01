4f35de834afb6b6fb8d8a74d14c167b92e6e03d2
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2022-04-01 16:41:15 -0300

    Obervable +  http-Post

--- src/app/services/food-list.service.ts
+++ src/app/services/food-list.service.ts
@@ -10,31 +10,27 @@ export class FoodListService {
 
   public emitEvent = new EventEmitter();
 
-  private list: Array<string> = [
-    "X Bacon",
-    "Feijão",
-    "Ovo"
-  ];
-
   //http://localhost:3000/list-food
   private url: string = "http://localhost:3000/";
 
   constructor(private http: HttpClient) { }
 
-  public foodList(): Observable<FoodList> {
-    return this.http.get<FoodList>(`${this.url}list-food`)
+  public foodList(): Observable<Array<FoodList>> {
+    return this.http.get<Array<FoodList>>(`${this.url}list-food`)
                       .pipe(
                         res => res,
                         error => error
                       );
   }
 
-  public foodListAdd(value: string){
-    this.foodListAlert(value);
-    return this.list.push(value);
+  public foodListAdd(value: string): Observable<FoodList>{
+    return this.http.post<FoodList>(`${this.url}list-food` , {nome: value}).pipe(
+      res => res,
+      error => error
+    )
   }
 
-  public foodListAlert(value: string){
+  public foodListAlert(value: FoodList){
     return this.emitEvent.emit(value);
   }
 
