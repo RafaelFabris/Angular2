9d426e7b14d0b61baec497c74dee976844d5ea09
Rafael Amaral Fabris <ra_rifabris@hotmail.com>
2022-04-01 14:55:11 -0300

    Service - Injeção de Dependencia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import {  Component, OnInit,} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: ' <<ng-template [*ngIf="getDados"]> <h1>{{getDados.nome}}</h1><h2>{{getDados.idade}}</h2> </ng-template>       <app-output (enviarDados)="setDados($event)"></app-output> <router-outlet></router-outlet>'
-  // <app-input [contador]="addValue"]> </app-input> <button (click)="add()")> Add </button>  <router-outlet></router-outlet>
+  template: '  <app-food-list></app-food-list>  <router-outlet></router-outlet>'
 })
 export class AppComponent implements OnInit{
 
